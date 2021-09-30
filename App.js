0c2d19447707150beec0a57010888aee2c04bca3
Emu <Emu@nest-56s-iMac.local>
2021-09-30 15:57:10 +0800

    done

--- App.js
+++ App.js
@@ -6,7 +6,6 @@
  * @flow strict-local
  */
 
-import {assertTSCallSignatureDeclaration} from '@babel/types';
 import React, {useRef, useState, useEffect} from 'react';
 import {
   Animated,
@@ -20,27 +19,28 @@ import {
   View,
   Dimensions,
 } from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 import Day from './svgs/Day.svg';
 import Night from './svgs/Night.svg';
 import DayBg from './svgs/daybg.svg';
 import NightBg from './svgs/nightbg.svg';
+import Sun from './svgs/sun.svg';
+import Stars from './svgs/stars.svg';
+import Moon from './svgs/moon.svg';
+import Clouds from './svgs/clouds.svg';
+import ShootingStar from './svgs/shoot.svg';
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 const App = () => {
+  //125 330
+  //15 -280
+  // 90 410
+  // 200 -200
   const isDarkMode = useColorScheme() === 'dark';
   const [darkMode, setDarkMode] = useState(isDarkMode);
-  const backgroundStyle = {
-    backgroundColor: darkMode ? Colors.darker : Colors.lighter,
-  };
   const animation = useRef(new Animated.Value(0)).current;
+  const sunLocation = useRef(new Animated.ValueXY({ x: 90, y: 410 })).current;
+  const moonLocation = useRef(new Animated.ValueXY({ x: 15, y: -280 })).current;
+  const shootLocation = useRef(new Animated.ValueXY({ x: 0, y: 0 })).current;
   const switchLocation = animation.interpolate({
     inputRange: [0, 1],
     outputRange: [0, 30],
@@ -53,38 +53,68 @@ const App = () => {
     inputRange: [0, 1],
     outputRange: [0, 0.25],
   });
-  const dayOpacity = animation.interpolate({
-    inputRange: [0, 1],
-    outputRange: [1, 0],
-  });
   const nightOpacity = animation.interpolate({
     inputRange: [0, 1],
     outputRange: [0, 1],
+  });
+  const dayOpacity = animation.interpolate({
+    inputRange: [0, 1],
+    outputRange: [1, 0],
   });
   const windowColor = animation.interpolate({
     inputRange: [0, 1],
     outputRange: ['#276B7E', '#FBE57F'],
   });
+  const cloudX = animation.interpolate({
+    inputRange: [0, 1],
+    outputRange: [0, -600]
+  });
   const toDark = () => {
     Animated.parallel([
-      Animated.sequence([
-        Animated.timing(animation, {
-          toValue: 1,
-          duration: 500,
-          useNativeDriver: false,
-        }),
-      ]),
+      Animated.timing(animation, {
+        toValue: 1,
+        duration: 1000,
+        useNativeDriver: false,
+      }),
+      Animated.timing(sunLocation, {
+        toValue: { x: 200, y: -200 },
+        duration: 1000,
+        useNativeDriver: false,
+      }),
+      Animated.timing(moonLocation, {
+        toValue: { x: 125, y: 330 },
+        duration: 1000,
+        useNativeDriver: false,
+      }),
+      Animated.timing(shootLocation, {
+        toValue: { x: 0, y: 0 },
+        duration: 1000,
+        useNativeDriver: false,
+      })
     ]).start();
   };
   const toLight = () => {
     Animated.parallel([
-      Animated.sequence([
-        Animated.timing(animation, {
-          toValue: 0,
-          duration: 500,
-          useNativeDriver: false,
-        }),
-      ]),
+      Animated.timing(animation, {
+        toValue: 0,
+        duration: 1000,
+        useNativeDriver: false,
+      }),
+      Animated.timing(sunLocation, {
+        toValue: { x: 90, y: 410 },
+        duration: 1000,
+        useNativeDriver: false,
+      }),
+      Animated.timing(moonLocation, {
+        toValue: { x: 15, y: 990 },
+        duration: 1000,
+        useNativeDriver: false,
+      }),
+      Animated.timing(shootLocation, {
+        toValue: { x: windowWidth, y: -200 },
+        duration: 1000,
+        useNativeDriver: false,
+      })
     ]).start();
   };
   useEffect(() => {
@@ -97,9 +127,6 @@ const App = () => {
       <Animated.View
         style={[
           styles.daynightbgcontainer,
-          {
-            opacity: dayOpacity,
-          },
         ]}>
         <DayBg width={windowWidth + 1} height={windowHeight + 1} />
       </Animated.View>
@@ -111,6 +138,11 @@ const App = () => {
           },
         ]}>
         <NightBg width={windowWidth + 1} height={windowHeight + 1} />
+      </Animated.View>
+      <Animated.View style={[styles.stars, {
+        opacity: nightOpacity,
+      }]}>
+        <Stars width={windowWidth} height={windowWidth*228/241}/>
       </Animated.View>
       <Pressable
         onPress={() => setDarkMode(!darkMode)}
@@ -143,12 +175,65 @@ const App = () => {
           />
         </Animated.View>
       </Pressable>
+      <Animated.View style={[styles.clouds, {
+        transform: [
+          {
+            translateX: cloudX
+          }
+        ]
+      }]}>
+        <Clouds width={windowWidth*1.4} height={windowWidth*125/370*1.4}/>
+      </Animated.View>
+      <Animated.View
+        style={[
+          styles.sun,
+          {
+            transform: [
+              {
+                translateX: sunLocation.x,
+              },
+              {
+                translateY: sunLocation.y,
+              }
+            ],
+            opacity: dayOpacity
+          }
+        ]}
+      >
+          <Sun width={94} height={95}/>
+      </Animated.View>
+      <Animated.View style={[
+        styles.moon,
+        {
+          transform: [
+            {
+              translateX: moonLocation.x,
+            }, {
+              translateY: moonLocation.y,
+            }
+          ],
+          opacity: nightOpacity,
+        }
+      ]}>
+        <Moon width={41*3/2} height={48*3/2}/>
+      </Animated.View>
+      <Animated.View style={[
+        styles.shootingstar,
+        {
+          transform: [
+            {
+              translateX: shootLocation.x,
+            }, {
+              translateY: shootLocation.y,
+            }
+          ],
+        }
+      ]}> 
+        <ShootingStar width={windowWidth/4*3} height={windowWidth/4*3*139/216}/>
+      </Animated.View>
       <Animated.View
         style={[
           styles.daynightContainer,
-          {
-            opacity: dayOpacity,
-          },
         ]}>
         <Day style={styles.daynight} width={windowWidth} />
       </Animated.View>
@@ -169,6 +254,7 @@ const App = () => {
           },
         ]}
       />
+      
     </SafeAreaView>
   );
 };
@@ -184,8 +270,8 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     position: 'absolute',
     right: 5,
-    top: 5,
-    zIndex: 10,
+    top: 50,
+    zIndex: 2,
   },
   switchDot: {
     height: 30,
@@ -205,7 +291,6 @@ const styles = StyleSheet.create({
     marginTop: 0,
     backgroundColor: '#27173A',
     borderRadius: 15,
-    zIndex: 1,
   },
   daynight: {
     position: 'absolute',
@@ -219,14 +304,12 @@ const styles = StyleSheet.create({
         translateY: -28,
       },
     ],
-    zIndex: 2,
   },
   daynightContainer: {
     flex: 1,
     position: 'absolute',
     right: 0,
     bottom: 0,
-    zIndex: 5,
   },
   window: {
     flex: 1,
@@ -235,7 +318,6 @@ const styles = StyleSheet.create({
     position: 'absolute',
     bottom: 304,
     right: 128,
-    backgroundColor: 'black',
   },
   daynightbgcontainer: {
     flex: 1,
@@ -243,6 +325,29 @@ const styles = StyleSheet.create({
     right: 0,
     bottom: 0,
   },
+  sun: {
+    flex: 1,
+    position: 'absolute',
+  },
+  stars: {
+    flex: 1,
+  },
+  moon: {
+    flex: 1,
+    position: 'absolute'
+  },
+  clouds: {
+    flex: 1,
+    position: 'absolute',
+    top: 200,
+    right: -100,
+  },
+  shootingstar: {
+    flex: 1,
+    position: 'absolute',
+    top: 390,
+    right: -20,
+  }
 });
 
 export default App;
